ed4360de480e8122afd6ec9c4ced3be58c0077d7
goodboychan <goodboychan@users.noreply.github.com>
2021-03-11 07:23:51 +0000

    deploy: cd798f7f86a4e9f7d39ae6a97fccb70cb43c7d61

--- assets/js/search.js
+++ assets/js/search.js
@@ -47,7 +47,7 @@ jtd.onReady = function(ready) {
 
 function initSearch() {
     var request = new XMLHttpRequest();
-    request.open('GET', '/chans_jupyter/assets/js/search-data.json', true);
+    request.open('GET', '/assets/js/search-data.json', true);
   
     request.onload = function(){
       if (request.status >= 200 && request.status < 400) {
